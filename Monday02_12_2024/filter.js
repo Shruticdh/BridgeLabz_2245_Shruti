c272c0b2ecb740c262fe14888ef7e5e966000e46
Shruticdh <shruticdh2003@gmail.com>
2024-12-03 15:18:50 +0530

    Shruti Add : Topics Covered on 03_12_2024

---
+++ Monday02_12_2024/filter.js
@@ -0,0 +1,3 @@
+const numbers = [1, 2, 3, 4];
+const evenNumbers = numbers.filter(num => num % 2 === 0); // Arrow function
+console.log(evenNumbers); // [2, 4]
---
+++ Monday02_12_2024/map.js
@@ -0,0 +1,3 @@
+const numbers = [1, 2, 3, 4];
+const doubled = numbers.map(num => num * 2); // Arrow function
+console.log(doubled); // [2, 4, 6, 8]
