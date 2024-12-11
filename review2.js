8e82c4933c8e6b8f975acc7bdee5a2c5b27471cd
Shruticdh <shruticdh2003@gmail.com>
2024-12-11 10:51:45 +0530

    [Shruti] Add : updated files

--- review2.js
+++ review2.js
@@ -5,7 +5,20 @@ for(let i=4;i>=0;i--){
 }
 console.log(arr2);
 
-for(let i=5;i<=arr.length;i++){
+for(let i=5;i<=arr.length-1;i++){
     arr2.push(arr[i]);
 }
 console.log(arr2);
+
+
+let arr4 = [];
+// console.log(arr4);
+for(let i=4;i>=0;i--){
+    arr4.push(arr[i]);
+}
+console.log(arr4);
+let arr5 = [...arr4 , ...arr.slice(5)];
+console.log(arr5);
+
+
+
