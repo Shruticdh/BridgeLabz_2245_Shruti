653c5d8bbef3bddf8cc25b42c9f8d9cd83e891f9
Shruticdh <shruticdh2003@gmail.com>
2024-12-20 11:06:13 +0530

    [Shruti] Add : new files

---
+++ Practice_DSA/Arrays/ImplementArray.js
@@ -0,0 +1,9 @@
+class customArray extends Array{
+
+}
+
+let arr= new customArray();
+arr.push(5);
+arr.push(5);
+arr.push(5);
+console.log(arr);
---
+++ review.js
@@ -0,0 +1,4 @@
+const txt = '{"name":"John", "age":30, "city":"New York"}';
+console.log(txt);
+const obj = JSON.parse(txt);
+console.log(txt);
