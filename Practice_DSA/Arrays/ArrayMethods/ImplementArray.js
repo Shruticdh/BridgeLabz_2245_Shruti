37d63a4d445f83829bfff1581a7189ed91e3ce76
Shruticdh <shruticdh2003@gmail.com>
2024-12-20 11:01:20 +0530

    [Shruti] Add : deleted unwated files

--- Practice_DSA/Arrays/ArrayMethods/ImplementArray.js
+++
@@ -1,9 +0,0 @@
-class customArray extends Array{
-
-}
-
-let arr= new customArray();
-arr.push(5);
-arr.push(5);
-arr.push(5);
-console.log(arr);
--- review.js
+++
@@ -1,4 +0,0 @@
-const txt = '{"name":"John", "age":30, "city":"New York"}';
-console.log(txt);
-const obj = JSON.parse(txt);
-console.log(txt);
